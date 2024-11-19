a91ec247dd6f860514c1e1bf071a262588d4fb36
s5720081 <s5720081@bournemouth.ac.uk>
2024-11-19 11:08:33 +0000

    Daily Sales function done

--- sales.py
+++ sales.py
@@ -19,5 +19,8 @@ available_items:(integer) This function returns this integer which updates the a
 The function will also update the inventory_records (For restocking) for a  given current day. 
 
     '''
-    
+    if current_day % 7 != 0:
+        sold_units = random.randrange(0,200)
+        available_items= available_items - sold_units
+        inventory_records.append([current_day, sold_units, 0, available_items])
     return available_items
